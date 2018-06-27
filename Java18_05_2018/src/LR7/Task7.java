1d58185ef231c485af44a0873624d94b408ae54e
YMnkf <yklesovay@gmail.com>
2018-06-27 21:36:50 +0600

    В LR7 добавлен п.7.

--- Java18_05_2018/src/LR7/Task7.java
+++ Java18_05_2018/src/LR7/Task7.java
@@ -13,26 +13,50 @@ public class Task7 {
   
     Scanner sc = new Scanner (System.in);
     String raw = sc.nextLine();
-    int a = sc.nextInt();
-    //int b = sc.nextInt();
+    int offset = sc.nextInt();
     
     String[] arr = raw.split(" ");
+    String[] arr1 = new String [arr.length + Math.abs(offset)];
        
 if (arr.length == 0) {
     System.out.print("Исходная строка пуста\n");
     return;
 }   
- 
-//String[] Arr = new String[]{"a","b","c", "d", "e", "f", "g", "h"};
 
-for (int i = 0; i < arr.length; i = i+1) {
-        if(i-a>=0) {
-            System.out.print(arr[i-a] + " ");
-        } else {
-            System.out.print(arr[(arr.length-1-(a-i-1))] + " ");
+int k = offset%arr.length;
+int m = (int) (offset/arr.length);
+
+
+if (offset>=0) {
+       for (int j = 0; j < k; j = j+1) {
+           arr1[j] = arr[arr.length-k+j];
+           System.out.print(arr1[j] + " ");
+       }
+        for (int j = k; j < arr1.length; j = j+1) {
+                   arr1[j] = arr[(int)((j-k)%arr.length)]; // нужно несколько раз написать arr слева
+                   System.out.print(arr1[j] + " ");
+            }
+        System.out.print("\n");
+        for (int i = 0; i < arr.length; i = i+1) {
+            arr[i] = arr1[i];
+            System.out.print(arr[i] + " ");
         }
-}            
-  
+}
+if (offset<0) {
+        for (int j = arr1.length-Math.abs(offset); j < arr1.length; j = j+1) {
+                   arr1[j] = arr[j%arr.length];
+                   System.out.print(arr1[j] + " ");
+            }
+        for (int j = 0; j < arr1.length-Math.abs(offset); j = j+1) {
+           arr1[j] = arr[(int)(j%arr.length)]; // нужно несколько раз написать arr слева
+           System.out.print(arr1[j] + " ");
+       }
+        System.out.print("\n");
+        for (int i = 0; i < arr.length; i = i+1) {
+            arr[i] = arr1[i+Math.abs(offset)];
+            System.out.print(arr[i] + " ");
+        }
+}
 System.out.print("\n");
-    }
+}
 }
