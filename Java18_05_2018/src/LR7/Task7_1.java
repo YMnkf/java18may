2c862db7dcfb949a99511bcd2a27072946900f45
YMnkf <yklesovay@gmail.com>
2018-06-28 18:08:13 +0600

    В LR7 добавлен п.7_1.

---
+++ Java18_05_2018/src/LR7/Task7_1.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LR7;
+
+import java.util.Scanner;
+
+public class Task7_1 {
+
+    public static void main(String[] args) {
+  
+    Scanner sc = new Scanner (System.in);
+    String raw = sc.nextLine();
+    int offset = sc.nextInt();
+    
+    String[] arr = raw.split(" ");
+    String[] arr1 = new String [arr.length];
+       
+if (arr.length == 0) {
+    System.out.print("Исходная строка пуста\n");
+    return;
+}   
+
+if (offset>=0) {
+       for (int i = 0; i < arr.length; i = i+1) {
+           arr1[(i+offset)%arr.length] = arr[i];
+       }
+       
+       for (int i = 0; i < arr1.length; i = i+1) {
+           System.out.print(arr1[i] + " ");
+       }
+       
+}
+
+if (offset<0) {
+       for (int i = 0; i < arr.length; i = i+1) {
+           if ((i+offset)>=0) {
+           arr1[(i+offset)%arr.length] = arr[i];
+           }
+           if ((i+offset)<0) {
+           arr1[(arr.length+i+offset%arr.length)%arr.length] = arr[i];
+           }
+       }
+   
+       
+       for (int i = 0; i < arr1.length; i = i+1) {
+           System.out.print(arr1[i] + " ");
+       }
+       
+}
+System.out.print("\n");
+}
+}
