8eccab8bcae1a0c093687f97fb592707e9f570d7
YMnkf <yklesovay@gmail.com>
2018-09-21 21:12:15 +0600

    Уточнен п.1 в LR11.

--- Java18_05_2018/src/LR11/Task1.java
+++ Java18_05_2018/src/LR11/Task1.java
@@ -11,90 +11,138 @@ import java.io.FileNotFoundException;
 
 public class Task1 {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
 
-        File target = new File ("./Resourses/task9299/test1.txt");
-        if (!target.exists()) {
-            System.out.println("Файл " + target.getAbsolutePath()+ " не существует" + "\n");
-            return;
+        try {
+            Task1Object currentObject = fileRead("./Resourses/task9299/test1.txt"); // открыли файл, как в текстовом документе. Курсор в самом начале
+            String [] arrSum = countSum(currentObject);
+
+            for (int i = 0; i < arrSum.length; i++) {
+                System.out.print(arrSum[i] + " ");
+            }
+
+
+        } catch (FileNotFoundException e) {
+            File currentFile = new File("./Resourses\task9299");
+            System.out.println("Файл не найден " + currentFile.getAbsolutePath());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
 
-        Scanner s = new Scanner (target); // открыли файл, как в текстовом документе. Курсор в самом начале
+        // Либо:
+        // public static void main(String[] args) throws FileNotFoundException {...
+        //System.out.print("\n");
+    }
+
+
+    public static Task1Object fileRead(String parthname) throws Exception {
+        Task1Object objectData;
+        objectData = new Task1Object();
+
+        File target = new File(parthname);
+        if (!target.exists()) {
+            //System.out.println("Файл " + target.getAbsolutePath()+ " не существует" + "\n");
+            //return;
+            Exception e = new Exception("Файл " + target.getAbsolutePath() + " не существует");
+            throw e;
+        }
+
+        Scanner s = new Scanner(target); // открыли файл, как в текстовом документе. Курсор в самом начале
 
         String lineStr;
         int m;
         int n;
         String[] arr;
-        
+
 
         if (s.hasNext()) {
             m = Integer.parseInt(s.nextLine());
-         } else {
-            System.out.print("Сообщение об ошибке" + "\n");
-            return;
+        } else {
+            //System.out.print("Сообщение об ошибке" + "\n");
+            //return;
+            Exception e = new Exception("Сообщение об ошибке");
+            throw e;
         }
 
-        if (m==0) { //Как добавить "Не является целым положит числом"?
-            System.out.print("Сообщение об ошибке" + "\n");
-            return;
+        if (m == 0) { //Как добавить "Не является целым положит числом"?
+            //System.out.print("Сообщение об ошибке" + "\n");
+            //return;
+            Exception e = new Exception("Сообщение об ошибке");
+            throw e;
         }
 
         if (s.hasNext()) {
             n = Integer.parseInt(s.nextLine());
-            //System.out.print(line);
         } else {
-            System.out.print("Сообщение об ошибке" + "\n");
-            return;
+            //System.out.print("Сообщение об ошибке" + "\n");
+            //return;
+            Exception e = new Exception("Сообщение об ошибке");
+            throw e;
         }
 
-        if (n==0) {
-            System.out.print("Сообщение об ошибке" + "\n");
-            return;
+        if (n == 0) {
+            //System.out.print("Сообщение об ошибке" + "\n");
+            //return;
+            Exception e = new Exception("Сообщение об ошибке");
+            throw e;
         }
 
-        double [][] arr1 = new double [m][n];
+        double[][] arr1 = new double[m][n];
 
-     for (int i=0; i<m; i++){
+        for (int i = 0; i < m; i++) {
             if (s.hasNext()) {///////////////////////////////////
                 arr = s.nextLine().split(" ");
 
-                for (int j=0; j<arr.length; j++){
-                    {arr1[i][j] = Double.parseDouble(arr[j]); ////////////////
+                for (int j = 0; j < arr.length; j++) {
+                    {
+                        arr1[i][j] = Double.parseDouble(arr[j]); ////////////////
+                    }
                 }
-               }
             } else {
-                System.out.print("Сообщение об ошибке" + "\n");
-                return;
+                //System.out.print("Сообщение об ошибке" + "\n");
+                //return;
+                Exception e = new Exception("Сообщение об ошибке");
+                throw e;
             }
 
             if (arr.length != n) {
-                System.out.print("Сообщение об ошибке" + "\n");
-                return;
+                //System.out.print("Сообщение об ошибке" + "\n");
+                //return;
+                Exception e = new Exception("Сообщение об ошибке");
+                throw e;
             }
+        }
+        s.close();   //Где это должно быть?
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr1[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+
+        objectData.arr1 = arr1;
+        objectData.m = m;
+        objectData.n = n;
+        return objectData;
+
     }
-    s.close ();   //Где это должно быть? 
-    
-     for (int i=0; i<m; i++){
-                for (int j=0; j<n; j++){
-                    System.out.print(arr1[i][j] + " ");
-                }
-                System.out.print("\n");
-               }
-    
-     String[] arrSum = new String [n];
-     
-     for (int j=0; j<n; j++){
-         double S = 0;
-         for (int i=0; i<m; i++){
-             S = S + arr1[i][j];
-         }
-             arrSum[j]= String.valueOf((double)S);
+
+
+    public static String[] countSum(Task1Object objectData) {
+
+
+        String[] arrSum = new String[objectData.n];
+
+        for (int j = 0; j < objectData.n; j++) {
+            double S = 0;
+            for (int i = 0; i < objectData.m; i++) {
+                S = S + objectData.arr1[i][j];
+            }
+            arrSum[j] = String.valueOf((double) S);
+
+        }
+
+        return arrSum;
     }
-     
-     for (int i=0; i<arrSum.length; i++){
-             System.out.print(arrSum[i] + " ");
-     }
-     
-     System.out.print("\n");
-}
 }
---
+++ Java18_05_2018/src/LR11/Task1Object.java
@@ -0,0 +1,8 @@
+package LR11;
+
+public class Task1Object {
+    double[][] arr1;
+    int n;
+    int m;
+
+}
